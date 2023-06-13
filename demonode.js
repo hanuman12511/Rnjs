cb0e087278d15aadca7702947aff6cb3deb1f6a7
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-06-13 15:57:04 +0530

    js

---
+++ demonode.js
@@ -0,0 +1,170 @@
+/* console.log("hello");
+var fname = "js"
+console.log(fname);
+var n1 = 5
+var n2 =8
+var r= n1+n2
+console.log(r);
+var r= n1-n2
+console.log(r);
+var r= n1*n2
+console.log(r);
+var r= n1/n2
+console.log(r);
+var r= n1%n2
+console.log(r);
+ */
+/* var n1 = 5
+ n1= n1+5
+console.log(n1);//10
+n1= n1-5
+console.log(n1);//5
+n1= n1*2
+console.log(n1); //10
+n1= n1/3
+console.log(n1);3.33
+n1=123
+n1= n1%10
+console.log(n1);//3 */
+
+/* var n1 = 5
+var n2 ='5'
+var r= n1>n2
+console.log(r);
+var r= n1<n2
+console.log(r);
+var r= n1>=n2
+console.log(r);
+var r= n1<=n2
+console.log(r);
+var r= n1!==n2
+console.log(r);
+var r= n1!=n2
+console.log(r);
+var r= n1==n2
+console.log(r);
+var r= n1===n2
+console.log(r); */
+
+/* var n1 =5
+var n2 =4
+var r =!(n1>10 || n2<20)
+console.log(r); */
+
+/* var n1=3
+n1--
+console.log(n1); */
+
+
+/* (false)? console.log("true"):console.log("false");
+var name1 =null
+ 
+name1 &&console.log(name1.length);
+ */
+
+/* pin = 2220
+if(pin==2222){
+    //true pin
+   console.log("login"); 
+}
+else if(pin==12345){
+    //true password
+    console.log("login");
+}
+else{
+    //false
+    console.log("not login");
+} */
+
+
+
+/* email ="Admin@gmail.com"
+if(email=="Admin@gmail.com"){
+    password =1222
+    if(password==1222){
+        console.log("login");
+    }
+    else{
+        console.log("not login");
+    }
+}
+
+else{
+    console.log("not login");
+} */
+/* 
+ch=3
+switch(ch){
+    case 1:console.log("add");
+        break;
+    
+    case 2:console.log("sub");
+        break;
+    
+    case 3:console.log("div");
+        break;
+    default:console.log("not use");
+} */
+
+/* 
+1 2 3 4 5 6 7 8 9 10
+
+1  1                 10
+
+i=1   i++          i<=10
+
+10  9   8  7  6   5  4   3   2  1
+
+i=10  i--                           i>=1 */
+
+//2 4 6 8 10
+/* for(i=1;i<=10; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+   
+}
+ */
+
+
+/* i=11
+while(i<=10){
+
+    console.log(i);
+    i++
+} */
+
+/* i=11
+do{
+
+    console.log(i);
+    i++
+}while(i<=10)
+
+ */
+
+
+/* num1 = 7
+
+num1=[2,3,4,5] array
+
+num1 ="1"
+num1=["user1","user2","user3"]
+
+num1[0] = "user1"
+num1[1] = "user2"
+num1[2] = "user3"
+ */
+
+/* num = [2,3,6,5,8]
+
+console.log(num);
+
+for(i=0;i<5;i++){
+    console.log(num[i]);
+}
+ */
+
+num = 23256
+
+[2,3,2,5,6]
